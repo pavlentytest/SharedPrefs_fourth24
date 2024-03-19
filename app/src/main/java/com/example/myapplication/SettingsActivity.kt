df90f3ce01f53990d40ccaa98b56a4a5eed14087
Pavel Stepanov <impetus@yandex.ru>
2024-03-19 18:31:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val map : Map<String,*> = sharedPreferences.all
+        map.forEach { e ->
+            Log.d("RRR","${e.key} - ${e.value}")
+        }
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
